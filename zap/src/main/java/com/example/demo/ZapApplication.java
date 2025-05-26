a2bf09ec95d264e8ccc6f8793a84745f7784ea1f
CJLEE\9ckdw <9ckdwls@naver.com>
2025-05-26 18:28:50 +0900

    최초 커밋

---
+++ zap/src/main/java/com/example/demo/ZapApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapApplication.class, args);
+	}
+
+}
